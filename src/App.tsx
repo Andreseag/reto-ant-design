dbe100dcf74824d1f452d0879b1915fad9a0c95b
Andreseag <castrogomezand@gmail.com>
2025-02-05 08:58:57 -0500

    feat: add admin config :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Login } from "./pages/Login";
+import { Admin } from "./pages/Admin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
