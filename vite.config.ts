e3dd5dfe08e4e2eefaa69d9f514e9f134483bd68
Andreseag <castrogomezand@gmail.com>
2025-02-05 12:10:11 -0500

    fix: fix erorr in limit build :sparkles:

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {
-      PUBLIC_KEY: JSON.stringify(process.env.PUBLIC_KEY),
-      PRIVATE_KEY: JSON.stringify(process.env.PRIVATE_KEY),
-    },
-  },
+  // define: {
+  //   "process.env": {
+  //     PUBLIC_KEY: JSON.stringify(process.env.PUBLIC_KEY),
+  //     PRIVATE_KEY: JSON.stringify(process.env.PRIVATE_KEY),
+  //   },
+  // },
+  build: { chunkSizeWarningLimit: 1600 },
 });
